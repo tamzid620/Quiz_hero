f486ca515e7630d36e361c28a0a963909ce0b1ee
tamzid620 <tamzid620@gmail.com>
2023-03-15 21:40:04 +0600

    countdown error solved

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ startQuiz.addEventListener("click", () => {
 
   let x = setInterval(() => {
     if (counterNum < 0) {
-      coutDown.classList.remove("flex");
-      coutDown.classList.add("hidden");
+      counter.classList.remove("flex");
+      counter.classList.add("hidden");
       counterNum = 3;
       count = 0;
       timer = null;
